971d33e395b118d66213599fd0df56b42a7e7410
GumBen7 <gumben7@gmail.com>
2023-09-10 01:41:36 +0900

    warnings solved

--- stack.h
+++ stack.h
@@ -1,11 +1,21 @@
+#include <stddef.h>
+#include <iostream>
+
 #include "node.h"
 
-class Stack{
+class Stack {
+private:
+    Node *top = nullptr;
+    size_t maxSize;
+    size_t size = 0;
+    value_type none; // TODO exit(1);
 public:
-    Stack();
+    Stack(size_t mS);
     Stack(const Stack& other);
     ~Stack();
 
     void push(const value_type& value);    
-    const value_type& pop();
+    value_type pop();
+    const value_type& peek() const;
 };
+
